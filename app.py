c1d1ece4d05c5790e7cc16d848171d854b5bea6b
Ethan05302 <zhan0915@algonquinlive.com>
2025-07-06 17:53:01 -0400

    Initial Lab 3 commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import logging
+import sys
+
+app = Flask(__name__)
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+
+@app.route('/')
+def home():
+    logging.info("Home endpoint accessed.")  
+    return "Flask Login App Lab 3 is Running."
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if username == "admin" and password == "password123":
+        logging.info(f"Login SUCCESS for user: {username}")
+        return jsonify({"message": "Login successful"}), 200
+    else:
+        logging.warning(f"Login FAILED for user: {username}")
+        return jsonify({"message": "Login failed"}), 401
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
